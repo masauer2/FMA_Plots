82467550ce55a3f56f38d064738e55a6202fe71c
masauer2 <masauer2@asu.edu>
2024-10-14 15:13:28 -0700

    Correct setup files

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='FMAPlots',
     version='0.1.0',
     packages=find_packages(include=['FMAPlots', 'FMAPlots.*']),
-    install_requires=['numpy>=1.14.5','matplotlib==3.9.2']
+    install_requires=['numpy>=1.14.5','matplotlib==3.9.2', 'pandas']
 )
